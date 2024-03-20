bfd39e43ec92e0dad233846e594c8e71a632d12c
Omer Faruk Oruc <omerfarukoruc35@gmail.com>
2024-03-20 16:39:18 +0300

    Update configuration.default.sdImage.nix

--- configuration.default.sdImage.nix
+++ configuration.default.sdImage.nix
@@ -1,121 +1,81 @@
 { config, pkgs, lib, ... }:
+{
 
-{  
-  # overlay = final: super: {
-  #   makeModulesClosure = x:
-  #   super.makeModulesClosure (x // { allowMissing = true; });
-  # };
-  # nixosHardwareVersion = "7f1836531b126cfcf584e7d7d71bf8758bb58969";
-  # imports = ["${fetchTarball "https://github.com/NixOS/nixos-hardware/archive/${nixosHardwareVersion}.tar.gz" }/raspberry-pi/4"];
   imports = [
     <nixpkgs/nixos/modules/installer/cd-dvd/sd-image-aarch64.nix>
+
+    # For nixpkgs cache
     <nixpkgs/nixos/modules/installer/cd-dvd/channel.nix>
   ];
+
   sdImage.compressImage = true;
-
-  fileSystems = {
-    "/" = {
-      device = "/dev/disk/by-label/NIXOS_SD";
-      fsType = "ext4";
-      options = [ "noatime" ];
-    };
-  };
-  # nixpkgs.overlays = [ overlay ];
-  config.system.build.sdImage.compressImage = false;
   
-  networking.networkmanager.enable = true;
-  services.openssh.enable = true;
-  time.timeZone = "Europe/Istanbul";
-  services.tailscale.enable = true;
-  hardware.raspberry-pi."4".fkms-3d.enable = true;
-  hardware.enableRedistributableFirmware = true;
-  nixpkgs.config.allowUnfree = true;
-  system.stateVersion = "23.11";
-  swapDevices = [{ device = "/swapfile"; size = 2048; }]; 
-  security.polkit.enable = true;
 
-  networking.firewall = {
-    enable = true;
-    trustedInterfaces = [ "tailscale0" ];
-    allowedUDPPorts = [ config.services.tailscale.port ];
-    allowedTCPPorts = [ config.services.tailscale.port 5901 ];
+  # NixOS wants to enable GRUB by default
+  boot.loader.grub.enable = false;
+  # Enables the generation of /boot/extlinux/extlinux.conf
+  boot.loader.generic-extlinux-compatible.enable = true;
+ 
+  # !!! Set to specific linux kernel version
+  boot.kernelPackages = pkgs.linuxPackages_5_4;
+
+  # !!! Needed for the virtual console to work on the RPi 3, as the default of 16M doesn't seem to be enough.
+  # If X.org behaves weirdly (I only saw the cursor) then try increasing this to 256M.
+  # On a Raspberry Pi 4 with 4 GB, you should either disable this parameter or increase to at least 64M if you want the USB ports to work.
+  boot.kernelParams = ["cma=256M"];
+
+  # Settings above are the bare minimum
+  # All settings below are customized depending on your needs
+
+  # systemPackages
+  environment.systemPackages = with pkgs; [ 
+    vim curl wget nano bind kubectl helm iptables openvpn
+    python3 nodejs-12_x docker-compose ];
+
+  services.openssh = {
+      enable = true;
+      permitRootLogin = "yes";
   };
 
-  environment.systemPackages = with pkgs; [
-    libraspberrypi raspberrypi-eeprom
-    curl
-    nixos-generators
-    xorg.xinit
-    glxinfo
-    vim
-    git
-    lshw
-    socat
-    chromium
-    nix-index
-    unzip zip 
-    wget
-    neatvnc turbovnc x11vnc 
-    xz
-    zlib
-    tailscale
-    geany
-    yazi alacritty alacritty-theme gnome.gnome-font-viewer gnome.gnome-logs gnome.gnome-system-monitor gnome.nautilus gnome.gnome-terminal
-    gnome.eog gnome.totem gnome-photos gnome.gnome-tweaks gnome.gnome-themes-extra gnome.gnome-system-monitor gnome.gnome-software gnome.gnome-shell-extensions
-    gnome.gnome-remote-desktop nautilus-open-any-terminal    
-  ];
+  programs.zsh = {
+      enable = true;
+      ohMyZsh = {
+          enable = true;
+          theme = "bira";
+      };
+  };
 
-  users = {
-    mutableUsers = false;
-    users."pi" = {
-      isNormalUser = true;
-      password = "admin";
-      extraGroups = [ "wheel" ];
+
+  virtualisation.docker.enable = true;
+
+  networking.firewall.enable = false;
+
+  # WiFi
+  hardware = {
+    enableRedistributableFirmware = true;
+    firmware = [ pkgs.wireless-regdb ];
+  };
+
+  # put your own configuration here, for example ssh keys:
+  users.defaultUserShell = pkgs.zsh;
+  users.mutableUsers = true;
+  users.groups = {
+    nixos = {
+      gid = 1000;
+      name = "nixos";
     };
   };
-
-  security.sudo.extraRules = [
-    {  users = [ "pi" ];
-       commands = [
-         { command = "ALL" ;
-           options= [ "NOPASSWD" ];
-        }
-      ];
-    }
-  ];
-
-  services.xserver = {
-    videoDrivers = [ "modesetting" ];
-    enable       = true;
-    xkbVariant = "";
-    layout = "en";
-    enableCtrlAltBackspace = true;
-    displayManager = {
-      autoLogin.enable = true;
-      autoLogin.user = "pi";
-      gdm.enable = true;
+  users.users = {
+    nixos = {
+      uid = 1000;
+      home = "/home/nixos";
+      name = "nixos";
+      group = "nixos";
+      shell = pkgs.zsh;
+      extraGroups = [ "wheel" "docker" ];
     };
-    desktopManager.gnome.enable = true;
   };
-
-  services.gnome.core-utilities.enable = false;
-
-  environment.shellAliases = {
-    raspi-cpu = ''
-      sudo vcgencmd get_throttled && sudo vcgencmd measure_temp
-    '';
-    raspi-firmware-update = ''
-      sudo mkdir -p /mnt && \
-      sudo mount /dev/disk/by-label/FIRMWARE /mnt && \
-      BOOTFS=/mnt FIRMWARE_RELEASE_STATUS=stable sudo -E rpi-eeprom-update -d -a && \
-      sudo umount /mnt
-    '';
-  };
-
-  services.xserver.displayManager.sessionCommands = ''
-     ${pkgs.x11vnc}/bin/x11vnc -rfbauth $HOME/.vnc/passwd -forever -shared -bg -display :0 &
-     '';
-}
-
-
-  
+  users.extraUsers.root.openssh.authorizedKeys.keys = [
+      # Your ssh key
+      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDqlXJv/noNPmZMIfjJguRX3O+Z39xeoKhjoIBEyfeqgKGh9JOv7IDBWlNnd3rHVnVPzB9emiiEoAJpkJUnWNBidL6vPYn13r6Zrt/2WLT6TiUFU026ANdqMjIMEZrmlTsfzFT+OzpBqtByYOGGe19qD3x/29nbszPODVF2giwbZNIMo2x7Ww96U4agb2aSAwo/oQa4jQsnOpYRMyJQqCUhvX8LzvE9vFquLlrSyd8khUsEVV/CytmdKwUUSqmlo/Mn7ge/S12rqMwmLvWFMd08Rg9NHvRCeOjgKB4EI6bVwF8D6tNFnbsGVzTHl7Cosnn75U11CXfQ6+8MPq3cekYr lucernae@lombardia-N43SM"
+  ];
