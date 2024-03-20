3992d7b45b7f9e010248599f0bde5e7788d50d66
Omer Faruk Oruc <omerfarukoruc35@gmail.com>
2024-03-20 16:06:54 +0300

    Update configuration.default.sdImage.nix

--- configuration.default.sdImage.nix
+++ configuration.default.sdImage.nix
@@ -17,9 +17,11 @@ in
 {
   # imports = ["${fetchTarball "https://github.com/NixOS/nixos-hardware/archive/${nixosHardwareVersion}.tar.gz" }/raspberry-pi/4"];
   imports = [
-    "${fetchTarball "https://github.com/NixOS/nixos-hardware/tarball/master"}/raspberry-pi/4"
     <nixpkgs/nixos/modules/installer/cd-dvd/sd-image-aarch64.nix>
-  ]; # switch upper import if gpu rendering feels slow.
+    <nixpkgs/nixos/modules/installer/cd-dvd/channel.nix>
+  ];
+
+  sdImage.compressImage = true;
 
   fileSystems = {
     "/" = {
